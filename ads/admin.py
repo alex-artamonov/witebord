9f427e8372bf157d425807ccb0ab9c2ab4e59a00
alex-artamonov <alex.artamonov2010@gmail.com>
2022-10-16 09:49:06 +0300

    configure admin

--- ads/admin.py
+++ ads/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 
 from .models import Guild, Ad
 
+
 # Register your models here.
+
+class AdsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'updated_at']
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+
 
 admin.site.register(Guild)
 
-admin.site.register(Ad)
+admin.site.register(Ad, AdsAdmin)
--- ads/models.py
+++ ads/models.py
@@ -3,39 +3,41 @@ from django.urls import reverse_lazy
 
 import users.models as um
 
+
 # Create your models here.
 
 class Guild(m.Model):
-    name = m.CharField(max_length=25, 
-        unique=True, 
-        blank=False,
-        null=False,
-        verbose_name="Наименование")
+    name = m.CharField(max_length=25,
+                       unique=True,
+                       blank=False,
+                       null=False,
+                       verbose_name="Наименование")
     description = m.TextField(verbose_name="Описание")
 
     def __str__(self):
         return self.name
 
     class Meta:
-            ordering = ['-name']
-            verbose_name = "Гильдия"
-            verbose_name_plural = 'Гильдии'
+        ordering = ['-name']
+        verbose_name = "Гильдия"
+        verbose_name_plural = 'Гильдии'
 
 
 class Ad(m.Model):
     title = m.CharField(
-        max_length=100, 
-        null=False, 
+        verbose_name='Заголовок',
+        max_length=100,
+        null=False,
         blank=False)
     content = m.TextField(null=False,
-        blank=False)
+                          blank=False)
     media_content = m.ImageField(upload_to="pics/%Y/%m/%d/", null=True, blank=True)
     created_at = m.DateTimeField(
-        auto_now_add=True, 
+        auto_now_add=True,
         verbose_name="Дата создания")
     updated_at = m.DateTimeField(
-    auto_now=True, 
-    verbose_name="Дата редактирования")
+        auto_now=True,
+        verbose_name="Дата редактирования")
 
     def __str__(self) -> str:
         return f"{self.title=}"
@@ -44,8 +46,8 @@ class Ad(m.Model):
         return self.content[:124] + '...'
 
     def get_absolute_url(self):
-        return reverse_lazy('ad_detail', kwargs = {'pk':self.id})
-    
+        return reverse_lazy('ad_detail', kwargs={'pk': self.id})
+
     class Meta:
         ordering = ['-created_at']
         verbose_name = "Объявление"
