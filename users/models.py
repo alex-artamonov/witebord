cedc0275fbf0135e49d2e6f1b1221d85167bb8bb
alex-artamonov <alex.artamonov2010@gmail.com>
2022-10-18 00:56:21 +0300

    change User model - modify Guild foreign key

--- users/models.py
+++ users/models.py
@@ -6,8 +6,11 @@ from ads.models import Guild
 # Create your models here.
 
 class User(AbstractUser):
-    guild = models.ForeignKey(to=Guild, null=True, on_delete=models.PROTECT)
-    pass
+    guild = models.ForeignKey(
+        to=Guild, 
+        null=True, 
+        blank=True,
+        on_delete=models.PROTECT)
 
     def __dir__(self):
         return self.username
