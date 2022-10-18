705107be8ef448374ce495d31a9154ebeaf76319
alex-artamonov <alex.artamonov2010@gmail.com>
2022-10-19 01:11:57 +0300

    add side bar, change defs to classes

--- ads/urls.py
+++ ads/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 import ads.views as av
 
 urlpatterns = [
-    path('ads', av.index, name='ads'),
+    # path('ads', av.index, name='ads'),
+    path('ads', av.AdsListView.as_view(), name='ads'),
     path('ads/<int:ad_id>', av.ad_detail, name='ad_detail'),
     path('guilds', av.GuildsListView.as_view(), name='guilds_list'),
     path('guilds/<int:pk>', av.GuildDetailView.as_view(), name='guild_detail'),
     path('accounts/<int:pk>', av.UserProfileView.as_view(), name='user_profile'),
-    path('rules', av.rules, name='rules' ),
+    path('rules', av.RulesView.as_view(), name='rules' ),
+    #  path('rules', av.rules, name='rules' ),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.views.generic import DetailView, ListView
+from django.db.models import Count
+
 import ads.models as ads
 import users.models as um
 
 
 # Create your views here.
+
+class CustomContextMixin():
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['guilds'] = ads.Guild.objects.annotate(cnt=Count('user'))
+        context['users'] = um.User.objects.all()
+        return context
 
 
 def index(request):
@@ -25,22 +34,38 @@ def ad_detail(request, ad_id):
     return render(request, 'ads/ad_detail.html', context)
 
 
-class GuildsListView(ListView):
+class AdsListView(CustomContextMixin, ListView):
+    model = ads.Ad
+    template_name = 'ads/ads.html'
+    context_object_name = 'ads_list'
+
+
+class GuildsListView(CustomContextMixin, ListView):
     model = ads.Guild
     template_name = 'ads/guilds_list.html'
     context_object_name = 'guilds_list'
 
 
-class GuildDetailView(DetailView):
+class GuildDetailView(CustomContextMixin, DetailView):
     model = ads.Guild
     template_name = 'ads/guild.html'
     context_object_name = 'guild'
 
-class UserProfileView(DetailView):
+class UserProfileView(CustomContextMixin, DetailView):
     model = um.User
     template_name = 'ads/profile.html'
     context_object_name = 'user'
 
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['guilds'] = ads.Guild.objects.all()
+    #     return context
+
 
 def rules(request):
     return render(request,'ads/rules.html')
+
+class RulesView(CustomContextMixin, ListView):
+    model = um.User
+    template_name = 'ads/rules.html'
+    # context_object_name = 'user'
