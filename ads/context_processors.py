cded8c370149b3c52bd00079b0f0fdb1fa2528c9
alex-artamonov <alex.artamonov2010@gmail.com>
2023-11-28 23:18:40 +0100

    small cleanup

--- ads/context_processors.py
+++ ads/context_processors.py
@@ -4,7 +4,6 @@ def search_form(request):
     frm = SearchForm(request.GET)
     query = ''
     if 'query' in request.GET and frm.is_valid:
-        query = 'query in request.GET and frm.is_valid'
         query = request.GET['query']
         # query = frm.cleaned_data
     return {'search_form': frm,
