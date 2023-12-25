7507ed5f3ec386de8e07924686ed3ffa6a0678fa
alex-artamonov <alex.artamonov2010@gmail.com>
2023-12-25 18:38:57 +0100

    Correct docker-compose.yml, command should be: python /code/manage.py runserver "0.0.0.0:8000", not just python /code/manage.py runserver, resulting in address 127.0.0.1:8000 Correct typo in DJANGO_SETTINGS_MODULE=Witebord.settings.local

--- Witebord/settings/prod.py
+++ Witebord/settings/prod.py
@@ -1,18 +1,26 @@
 from .base import *
 
+ALLOWED_HOSTS = ['localhost', 
+                 '127.0.0.1', 
+                 '0.0.0.0:8000']
 DEBUG = False
 
 ADMINS = [
     ('Alex Artamonov', 'alex.artamonov2010@gmail.com'),
 ]
-ALLOWED_HOSTS = ['*']
+
+#DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'witeboard2',
+#         'USER': 'witeboard',
+#         'PASSWORD': '12345',
+#         'PORT': '5432',
+#         'HOST': '0.0.0.0',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'witeboard2',
-        'USER': 'witeboard',
-        'PASSWORD': '12345',
-        'PORT': '5432',
-        'HOST': 'localhost',
-    }
+'default': {
+}
 }
