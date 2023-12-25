54d6e7b36b698123cc52476a83fabc0bb4c91c23
alex-artamonov <alex.artamonov2010@gmail.com>
2023-12-25 20:04:13 +0100

    Correct docker-compose.yml: PORTS are important

--- Witebord/settings/prod.py
+++ Witebord/settings/prod.py
@@ -1,15 +1,26 @@
+import os
 from .base import *
 
-ALLOWED_HOSTS = ['localhost', 
-                 '127.0.0.1', 
-                 '0.0.0.0:8000']
 DEBUG = False
 
 ADMINS = [
     ('Alex Artamonov', 'alex.artamonov2010@gmail.com'),
 ]
 
-#DATABASES = {
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
+
+# DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME': 'witeboard2',
@@ -20,7 +31,3 @@ ADMINS = [
 #     }
 # }
 
-DATABASES = {
-'default': {
-}
-}
