c1e3478b55587b1cc218667412bc573e1656f106
alex-artamonov <alex.artamonov2010@gmail.com>
2023-12-28 00:40:22 +0100

    small clean up

--- Witebord/test-env.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-api_key = os.getenv("API_KEY")
-api_secret = os.getenv("API_SECRET")
-# print(api_key, api_secret, os.getenv("EMAIL_PORT"))
