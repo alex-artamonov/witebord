086ac76f88655766423826a8ca4c1fc6c22ad66e
alex-artamonov <alex.artamonov2010@gmail.com>
2023-12-28 00:37:59 +0100

    small clean up

--- Witebord/test-env.py
+++ Witebord/test-env.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 api_key = os.getenv("API_KEY")
 api_secret = os.getenv("API_SECRET")
-print(api_key, api_secret, os.getenv("EMAIL_PORT"))
+# print(api_key, api_secret, os.getenv("EMAIL_PORT"))
