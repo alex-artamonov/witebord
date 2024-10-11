e45dd55d2a2f6bebc3c0bbfa15b4982ec6f16d16
alex-artamonov <alex.artamonov2010@gmail.com>
2024-10-11 19:03:45 +0200

    testing .gitconfig again

--- Witebord/settings.py
+++ Witebord/settings.py
@@ -191,9 +191,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379',
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+#         'LOCATION': 'redis://127.0.0.1:6379',
+#     }
+# }
